479ab369a97f55a32ab73b03720f1a7df19d437d
ryo1113 <53460858+ryo1113@users.noreply.github.com>
2019-11-08 11:17:51 +0900

    改善４

---
+++ team_Prototype/code/model.h
@@ -0,0 +1,67 @@
+//=============================================================================
+//
+// モデル処理 [model.h]
+// Author : KOUTA KIMURA
+//
+//=============================================================================
+#ifndef _MODEL_H_
+#define _MODEL_H_
+
+#define _CRT_SECURE_NO_WARNINGS
+
+//=============================================================================
+// インクルードファイル
+//=============================================================================
+#include "main.h"
+#include "Scene3D.h"
+
+//=============================================================================
+// 構造体定義
+//=============================================================================
+
+//=============================================================================
+// マクロ定義
+//=============================================================================
+#define MODELFILE0		"DATA/MODEL/stage.x"					// 読み込むモデル
+
+#define MAX_TEXTURE		(21)									// テクスチャ数
+#define MAX_SET_TEXTURE	(20)									// 設置テクスチャ数
+
+//================================================================
+// レンダリングクラス
+//================================================================
+class CModel : public CScene3D
+{
+public:
+	enum UNITTYPE
+	{
+		UNITTYPE_FLOOR,
+		UNITTYPE_MAX
+	};
+
+	//=============================================================================
+	// プロトタイプ宣言
+	//=============================================================================
+	CModel(OBJTYPE type);
+	~CModel();
+
+	static CModel *Create(D3DXVECTOR3 pos, D3DXVECTOR3 rot, UNITTYPE type);
+
+	HRESULT Init(D3DXVECTOR3 pos, D3DXVECTOR3 rot, UNITTYPE type);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	static HRESULT Load(void);
+	static void Unload(void);
+	//void SetModel(D3DXVECTOR3 pos, D3DXVECTOR3 rot, MODELTYPE type);
+	//bool CollisionModel(D3DXVECTOR3 *pPos, D3DXVECTOR3 *pPosOld, D3DXVECTOR3 size);
+
+private:
+	static MODELNUM m_Unit[UNITTYPE_MAX];
+
+	LPDIRECT3DTEXTURE9	m_pTextureModel[MAX_TEXTURE] = {};			// テクスチャへのポインタ
+	D3DXVECTOR3 m_pos;
+
+};
+#endif
